8c8b11113cbcc7a0730fb5fc1f19547319489b13
natashamorais <natasha.sousa.morais@gmail.com>
2018-12-05 18:12:18 -0200

    Atualização da função funcionando

--- src/components/TextTyping.js
+++ src/components/TextTyping.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
+// import Page1 from '../pages/page1'
 
-class TextTyping extends React.Component {
+class TextTyping extends Component {
 
   state = {
     typing: '',
@@ -8,22 +10,31 @@ class TextTyping extends React.Component {
 
   componentDidMount() {
 
-      const splitText = (this.props.text).split("");
+    const splitText = (this.props.text).split("");
 
 
-        splitText.forEach((characters, index) => {
+    splitText.forEach((characters, index) => {
 
-          return setTimeout(() =>
-          
-          console.log(characters), 500 * index);
-        
+      return setTimeout(() => {
+        this.setState({
+          typing: this.state.typing += characters
         });
+
+        if (index === splitText.length - 1) {
+          setTimeout(() => {
+            if(this.props.router !== undefined){
+              this.props.history.push(this.props.router)
+            }
+          }, 2000);
+        }
+
+      }, 100 * index);
+
+    });
   }
-
-
   render() {
     return <h1>{this.state.typing}</h1>
   }
 }
 
-export default TextTyping;
+export default withRouter(TextTyping);
