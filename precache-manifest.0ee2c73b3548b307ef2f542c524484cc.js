db8eb73e1f0c2de0e71e4596457f91376ed5b29d
natashamorais <natasha.sousa.morais@gmail.com>
2018-12-07 10:36:25 -0200

    Updates

---
+++ precache-manifest.0ee2c73b3548b307ef2f542c524484cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd9e52dab98d64d94b3",
+    "url": "/react-gordofobia-banal-/static/js/runtime~main.cfd9e52d.js"
+  },
+  {
+    "revision": "8104fd3f09db2e6dce28",
+    "url": "/react-gordofobia-banal-/static/js/main.8104fd3f.chunk.js"
+  },
+  {
+    "revision": "25e58ae8da6babdfca7d",
+    "url": "/react-gordofobia-banal-/static/js/1.25e58ae8.chunk.js"
+  },
+  {
+    "revision": "8104fd3f09db2e6dce28",
+    "url": "/react-gordofobia-banal-/static/css/main.3573fb96.chunk.css"
+  },
+  {
+    "revision": "71041cf571b4d804108a3fd67b939714",
+    "url": "/react-gordofobia-banal-/index.html"
+  }
+];
